14129aa24bc84a34c124314c7be8a7229a0bb5bd
William Krug <william.p.krug@gmail.com>
2021-02-17 16:08:26 -0600

    sql database creation and script formatting

--- database.sql
+++ database.sql
@@ -1,10 +1,15 @@
 -- CREATE DATABASE "prim-proper-props"
+DROP TABLE "guests";
 
-CREATE TABLE guests (
+CREATE TABLE "guests" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(200) NOT NULL,
     "kidsMeal" BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO guests ("name", "kidsMeal")
-VALUES ('Edan', false), ('Dane', false), ('Winnie', true);
+INSERT INTO "guests"
+  ("name", "kidsMeal")
+VALUES
+  ('Edan', false),
+  ('Dane', false),
+  ('Winnie', true);
