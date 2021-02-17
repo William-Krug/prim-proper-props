511a6d9e210b13faa40ef0f95c0684571ff37a98
William Krug <william.p.krug@gmail.com>
2021-02-17 16:56:37 -0600

    wrapping return in <div>

--- src/components/PartyLeader/PartyLeader.js
+++ src/components/PartyLeader/PartyLeader.js
@@ -1,10 +1,10 @@
 function PartyLeader({ leader }) {
   console.log('leader:', leader);
   return (
-    <>
+    <div>
       <h2>Party Leader</h2>
       <h3>{leader.name}</h3>
-    </>
+    </div>
   );
 }
 
