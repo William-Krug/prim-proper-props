5c20d400db18bc02da6c0e16e81db052f44c040d
William Krug <william.p.krug@gmail.com>
2021-02-17 18:46:41 -0600

    added DELETE path for axios

--- src/components/GuestList/GuestList.js
+++ src/components/GuestList/GuestList.js
@@ -7,7 +7,7 @@ function GuestList({ guestList }) {
           <tr>
             <th>Name</th>
             <th>Kid's Meal</th>
-            <th>Remove Guest</th>
+            <th>Remove</th>
           </tr>
         </thead>
         <tbody>
