d7904ba647b7443e927334982304a4cebf609bfb
William Krug <william.p.krug@gmail.com>
2021-02-17 18:05:52 -0600

    added notes to why "leader" needs to be attached to <h3> in ParyLeader

--- src/components/PartyLeader/PartyLeader.js
+++ src/components/PartyLeader/PartyLeader.js
@@ -4,7 +4,16 @@ function PartyLeader({ leader }) {
   return (
     <div>
       <h2>Party Leader</h2>
+      {/* 
+          Make sure leader exists
+          and if it does, then render
+          to DOM
+       */}
       {leader && <h3>{leader.name}</h3>}
+      {/* 
+          this may not work if .name
+          doesn't exist for a split second
+       */}
       {/* <h3>{leader.name}</h3> */}
     </div>
   );
